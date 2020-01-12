2258de1af567b4521e9c7f8067e9868ae663703a
Hamed Zarei <hamedzarei.7232@gmail.com>
2020-01-12 15:16:10 +0330

    Update shecan.sh

--- shecan.sh
+++ shecan.sh
@@ -2,5 +2,5 @@
 t=/etc/resolv.conf
 sudo sed -i "/nameserver 185.51.200.2/d" $t
 sudo sed -i "/nameserver 178.22.122.100/d" $t
-echo "nameserver 185.51.200.2" >> $t
-echo "nameserver 178.22.122.100" >> $t
+echo "nameserver 185.51.200.2" | sudo tee -a $t
+echo "nameserver 178.22.122.100" | sudo tee -a $t
